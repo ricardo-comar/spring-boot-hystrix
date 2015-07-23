a033f5149d2388e9a762ac0cfedddeb6992080b6
Ricardo Comar <rcomar@ciandt.com>
2015-07-23 10:44:17 -0300

    first commit

---
+++ src/main/java/br/comar/ricardo/stuff/boot/hystrix/SpringBootHystrixApplication.java
@@ -0,0 +1,12 @@
+package br.comar.ricardo.stuff.boot.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHystrixApplication.class, args);
+    }
+}
---
+++ src/test/java/br/comar/ricardo/stuff/boot/hystrix/SpringBootHystrixApplicationTests.java
@@ -0,0 +1,16 @@
+package br.comar.ricardo.stuff.boot.hystrix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringBootHystrixApplication.class)
+public class SpringBootHystrixApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
